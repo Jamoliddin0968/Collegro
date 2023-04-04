1a54cc9de7cacea0e5df173d06560be73ee2d919
Jamoliddin Ibrohimov <jamoliddin0968@gmail.com>
2023-04-04 15:11:22 +0500

    auth

--- config/settings.py
+++ config/settings.py
@@ -116,11 +116,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / 'static_files/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
-
+MEDIA_URL = '/media/'
 AUTH_USER_MODEL = "users.user"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
